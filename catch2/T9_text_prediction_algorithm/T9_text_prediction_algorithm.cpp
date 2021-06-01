15971d0d52b5579b95761f6af730d4b2e77f1801
Shajeen Ahamed <2623563+shajeen@users.noreply.github.com>
2021-06-01 19:11:46 +0530

    added catch2
    plain project added

---
+++ catch2/T9_text_prediction_algorithm/T9_text_prediction_algorithm.cpp
@@ -0,0 +1,11 @@
+﻿// T9_text_prediction_algorithm.cpp : Defines the entry point for the application.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "..\catch.hpp"
+
+TEST_CASE("")
+{
+
+}
---
+++ catch2/failed_testcase/failed_testcase.h
@@ -0,0 +1,13 @@
+﻿// failed_testcase.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+namespace TestCase {
+class FailedClass {
+public:
+  int failedRet() const { return 42; }
+};
+} // namespace TestCase
