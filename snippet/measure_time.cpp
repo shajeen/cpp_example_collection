1ff03d4f05d643f9733982c101984bb092eb1d78
Shajeen Ahamed <shajeenahmed@gmail.com>
2022-11-17 20:33:02 +0530

    measure timer added

--- snippet/measure_time.cpp
+++ snippet/measure_time.cpp
@@ -11,9 +11,24 @@ public:
         this->start = std::chrono::high_resolution_clock::now();
     }
 
-    void stopMillisecond(std::string &&message)
+    void stopMilliSecond(std::string &&message)
     {
         std::cout << message << " : " << std::chrono::duration_cast<std::chrono::milliseconds>(stopTimer() - this->start) << std::endl;
+    }
+
+    void stopMicroSecond(std::string &&message)
+    {
+        std::cout << message << " : " << std::chrono::duration_cast<std::chrono::microseconds>(stopTimer() - this->start) << std::endl;
+    }
+
+    void stopNanoSecond(std::string &&message)
+    {
+        std::cout << message << " : " << std::chrono::duration_cast<std::chrono::nanoseconds>(stopTimer() - this->start) << std::endl;
+    }
+
+    void startAgain()
+    {
+        this->start = std::chrono::high_resolution_clock::now();
     }
 
 private:
@@ -29,7 +44,15 @@ int main()
 {
     MeasureTime timer;
     std::this_thread::sleep_for(std::chrono::seconds(10));
-    timer.stopMillisecond("10 second timer");
+    timer.stopMilliSecond("10 second in millisecond");
+
+    timer.startAgain();
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    timer.stopMicroSecond("10 second in microsecond");
+
+    timer.startAgain();
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    timer.stopNanoSecond("10 second in nanosecond");
 
     return 0;
 }
